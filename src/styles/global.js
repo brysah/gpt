00104b7db7bf185d89620654844099719a1f781b
brysah <brysahvidal@gmail.com>
2024-04-12 13:46:20 -0300

    feat: add theme and global style

---
+++ src/styles/global.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-decoration: none;
+    outline: none;
+    border: none;
+    box-sizing: border-box;
+    font-family: 'Inter',sans-serif;
+}
+
+img{
+    max-width: 100%;
+    display: block;
+}
+
+html{
+    font-size: 62.5%;
+}
+
+button{
+    background-color: transparent;
+    cursor: pointer;
+} 
+`
---
+++ src/styles/theme.js
@@ -0,0 +1,21 @@
+export default{
+    COLORS:{
+        BACKGROUND:"#040C18",
+        BACKGROUND_CARD:"#042C54",
+        BACKGROUND_FOOTER:"#031B34",
+
+        TEXT_MENU:"#fff",
+        BACKGROUND_BUTTON_MENU:"#FF4820",
+
+        TEXT_PARAGRAPH:"#81AFDD",
+        TEXT_SPAN:"#71E5FF",
+
+        TEXT_INPUT:"#3D6184",
+        BACKGROUND_INPUT:"#052D56",
+        BACKGROUND_BUTTON_INPUT:"#FF4820",
+
+        TEXT_LINK:"#FF8A71",
+
+        TEXT_FOOTER:"#fff"
+    }
+}
