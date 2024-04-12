7baefbd32c375d4009c49d8e60d6bc233591b0aa
brysah <brysahvidal@gmail.com>
2024-04-12 19:00:39 -0300

    feat: add section what-is

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,10 @@
 import { Header } from "../../components/Header"
 import { Section } from '../../components/Section'
-import { Hero,Companies } from "./styles"
+import { Hero, Companies, Info } from "./styles"
 import { Input } from "../../components/Input"
 import group_people from '../../assets/group-people.svg'
-import { company,company2,company3,company4,company5 } from '../../utils/companies'
+import { company, company2, company3, company4, company5 } from '../../utils/companies'
+import { Link } from 'react-router-dom'
 
 export function Home() {
     return (
@@ -14,13 +15,13 @@ export function Home() {
                     <div className="right-area">
                         <h1>Let’s Build Something amazing with GPT-3 OpenAI</h1>
                         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-                        <Input/>
+                        <Input />
                         <div className="request-acess">
                             <img src={group_people} alt="users" className="group-people" />
                             <p>1,600 people requested access a visit in last 24 hours</p>
                         </div>
                     </div>
-                    <div className="left-area"> 
+                    <div className="left-area">
                     </div>
                 </Hero>
             </Section>
@@ -32,6 +33,53 @@ export function Home() {
                     <img src={company4} alt="logo dropbox" />
                     <img src={company5} alt="logo shopify" />
                 </Companies>
+            </Section>
+            <Section>
+                <Info>
+                    <div className="top">
+                        <div className="title">
+                            <div className="line"></div>
+                            <h3>What is GPT-3</h3>
+                        </div>
+                        <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
+                    </div>
+                    <div className="content">
+                        <div className="explore">
+                            <h4>The possibilities are beyond your imagination</h4>
+
+                            <Link>Explore The Library </Link>
+                        </div>
+                        <div className="cards">
+                            <div className="card">
+                                <div className="title">
+                                    <div className="line"></div>
+                                    <h3>Chatbots</h3>
+                                </div>
+                                <p>
+                                    We so opinion friends me message as delight. Whole front do of plate heard oh ought.
+                                </p>
+                            </div>
+                            <div className="card">
+                                <div className="title">
+                                    <div className="line"></div>
+                                    <h3>Knowledgebase</h3>
+                                </div>
+                                <p>
+                                    At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b
+                                </p>
+                            </div>
+                            <div className="card">
+                                <div className="title">
+                                    <div className="line"></div>
+                                    <h3>Education</h3>
+                                </div>
+                                <p>
+                                    At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </Info>
             </Section>
         </>
     )
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,16 +1,16 @@
-import styled from "styled-components"; 
-import illustration from '../../assets/hero-illustration.png'
+import styled from "styled-components";
+import illustration from "../../assets/hero-illustration.png";
 
-export const Hero = styled.div` 
+export const Hero = styled.div`
   min-height: 70rem;
   overflow-x: hidden;
-  display: flex; 
-  > .left-area { 
+  display: flex;
+  > .left-area {
     max-width: 60rem;
     transform: translateX(10rem);
     height: 66rem;
     width: 100%;
-    background-image: url(${illustration}) ;
+    background-image: url(${illustration});
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
@@ -52,19 +52,19 @@ export const Hero = styled.div`
       }
     }
   }
-  @media (max-width:1024px){
+  @media (max-width: 1024px) {
     flex-wrap: wrap;
     align-items: center;
     justify-content: center;
     flex-direction: column-reverse;
-    >.left-area{ 
+    > .left-area {
       transform: translateX(0);
       height: 50rem;
     }
-    >.right-area{ 
+    > .right-area {
       text-align: center;
       margin-top: 5rem;
-      .request-acess{
+      .request-acess {
         justify-content: center;
         flex-wrap: wrap;
       }
@@ -79,15 +79,109 @@ export const Companies = styled.div`
   justify-content: space-between;
   align-items: center;
   margin: 12rem auto 12.6rem;
-  >img{
+  > img {
     max-width: 7.1rem;
   }
-  @media (max-width:1024px){
+  @media (max-width: 1024px) {
     margin-bottom: 6rem;
     flex-wrap: wrap;
   }
-  @media (max-width:670px){
+  @media (max-width: 670px) {
     justify-content: center;
     gap: 2rem;
   }
-`
+`;
+
+export const Info = styled.div`
+  background: ${({ theme }) => theme.COLORS.BACKGROUND_CARD};
+  padding: 5.7rem 6.1rem;
+  width: 100%;
+  margin-bottom: 20rem;
+  .top {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 9.1rem;
+    flex-wrap: wrap;
+    gap: 2rem;
+    p {
+      color: ${({ theme }) => theme.COLORS.TEXT_PARAGRAPH};
+      font-size: 1.6rem;
+      font-weight: 500;
+      line-height: 3rem;
+      max-width: 73.1rem;
+      width: 100%;
+    }
+  }
+  .title {
+    .line {
+      width: 3.8rem;
+      height: 0.3rem;
+      background: linear-gradient(103deg, #ae67fa -13.86%, #f49867 99.55%), #fff;
+    }
+    h3 {
+      color: #fff;
+      font-size: 2.4rem;
+      font-weight: 800;
+      line-height: 7.5rem;
+      letter-spacing: -0.096rem;
+    }
+  }
+  .content {
+    .explore {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 9.7rem;
+      align-items: flex-start;
+      h4 {
+        background: linear-gradient(90deg, #ae67fa 1.84%, #f49867 102.67%);
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-size: 3.4rem;
+        font-weight: 800;
+        line-height: 4.5rem;
+        max-width: 47.2rem;
+        width: 100%;
+      }
+      a {
+        color: ${({ theme }) => theme.COLORS.TEXT_LINK};
+        font-size: 1.6rem;
+        font-weight: 500;
+        line-height: 3rem;
+      }
+    }
+    .cards {
+      display: flex;
+      justify-content: space-between;
+      gap: 1.5rem;
+      .card {
+        max-width: 33.4rem;
+        width: 100%;
+        p {
+          color: ${({ theme }) => theme.COLORS.TEXT_PARAGRAPH};
+          font-size: 1.6rem;
+          font-weight: 500;
+          line-height: 3rem;
+        }
+      }
+    }
+  }
+
+  @media (max-width: 1024px) {
+    padding: 3rem;
+    .content {
+      .explore{
+        flex-wrap: wrap;
+        align-items: center;
+        gap: 2rem;
+      }
+      .cards {
+        flex-wrap: wrap;
+        gap: 2rem;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+  }
+`;
