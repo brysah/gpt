6d684ed83ca08aa8ddc5f1bb06b73af9b6833d72
brysah <brysahvidal@gmail.com>
2024-04-12 16:27:10 -0300

    feat: responsive header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,71 @@
-import { Container, Navigation, Button } from "./styles";
+import { Container, Navigation, Button, Blur, Mobile, ButtonMobile } from "./styles";
 import logo from '../../assets/logo.svg'
+import blur from '../../assets/blur-01.png'
 import { Link } from 'react-router-dom'
+import { IoIosMenu } from "react-icons/io";
 
-export function Header() {
+
+export function Header() {  
+    
+    const mobileID = Mobile.componentStyle.componentId; 
+
+    function openMenu() {
+        document.body.style.overflowY = 'hidden';
+        document.querySelector(`.${mobileID}`).style.opacity = 1;
+        document.querySelector(`.${mobileID}`).style.pointerEvents = 'all';
+        document.querySelector(`aside`).style.transform = 'translateX(0%)'; 
+    }
+
+    function closeMenu() {
+        document.body.style.overflowY = 'auto'; 
+        document.querySelector(`.${mobileID}`).style.opacity = 0;
+        document.querySelector(`.${mobileID}`).style.pointerEvents = 'none';
+        document.querySelector(`aside`).style.transform = 'translateX(-100%)'; 
+    }
+ 
     return (
-        <Container>
-            <Link> <img src={logo} alt="Logo GPT-3" /> </Link>
-            <Navigation>
-                <ul>
-                    <li><Link to="/"> Home </Link></li>
-                    <li><Link to=""> What is GPT? </Link></li>
-                    <li><Link to=""> Open AI </Link></li>
-                    <li><Link to=""> Case Studies </Link></li>
-                    <li><Link to=""> Library</Link></li>
-                </ul>
+        <>
+            <Container>
+                <Blur>
+                    <img src={blur} alt="Blur" />
+                </Blur>
+                <Link> <img src={logo} alt="Logo GPT-3" /> </Link>
+                <Navigation>
+                    <ul>
+                        <li><Link to="/"> Home </Link></li>
+                        <li><Link to=""> What is GPT? </Link></li>
+                        <li><Link to=""> Open AI </Link></li>
+                        <li><Link to=""> Case Studies </Link></li>
+                        <li><Link to=""> Library</Link></li>
+                    </ul>
 
-                <div>
-                    <Link to="">Sign In</Link>
-                    <Button>Sign Up</Button>
-                </div>
-            </Navigation>
-        </Container>
+                    <div>
+                        <Link to="">Sign In</Link>
+                        <Button>Sign Up</Button>
+                    </div>
+                </Navigation>
+
+                <ButtonMobile onClick={openMenu}>
+                    <IoIosMenu />
+                </ButtonMobile>
+            </Container>
+
+            <Mobile>
+                <div className="overlay" onClick={closeMenu}></div>
+                <aside>
+                    <Link> <img src={logo} alt="Logo GPT-3" /> </Link>
+                    <ul>
+                        <li><Link to="/"> Home </Link></li>
+                        <li><Link to=""> What is GPT? </Link></li>
+                        <li><Link to=""> Open AI </Link></li>
+                        <li><Link to=""> Case Studies </Link></li>
+                        <li><Link to=""> Library</Link></li>
+                        <li><Link to=""> Sign In</Link></li>
+                        <Button>Sign Up</Button>
+                    </ul>
+                </aside>
+            </Mobile>
+        </>
+
     )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,7 +1,6 @@
-import styled from 'styled-components'
+import styled from 'styled-components' 
 
-
-export const Container = styled.header`
+export const Container = styled.header` 
     width: 100%;
     display: flex;
     align-items: center;
@@ -13,6 +12,9 @@ export const Container = styled.header`
         max-width: 6.2rem;
         width: 100%;
     }
+    @media (max-width:1024px){
+        justify-content: space-between;
+     }
 `
 
 export const Navigation = styled.nav`
@@ -37,6 +39,9 @@ export const Navigation = styled.nav`
         display: flex;
         gap: 2.6rem;
         align-items: center;
+     } 
+     @media (max-width:1024px){
+        display: none;
      }
 `
 
@@ -52,5 +57,84 @@ export const Button = styled.button`
      &:hover{
         background-color:  ${({ theme }) => theme.COLORS.TEXT_MENU}; 
         color: ${({ theme }) => theme.COLORS.BACKGROUND_BUTTON_MENU};
+     } 
+`
+
+export const ButtonMobile = styled.button`
+    display: none;
+    >svg{
+        color: #FFF;
+        font-size: 5rem;
+    }
+    @media (max-width:1024px){
+        display: block;
      }
 `
+
+export const Mobile = styled.div` 
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 20;
+    opacity: 0;
+    pointer-events: none;
+    transition: opacity .3s;
+    
+    >.overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        z-index: 0;
+    }
+    >aside{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 75%;
+        height: 100%;
+        background: ${({ theme }) => theme.COLORS.BACKGROUND}; 
+        padding: 3rem;
+        transform: translateX(-100%);
+        transition: transform .3s;
+        >img {
+            max-width: 15rem;
+            margin-bottom: 4rem;
+        }
+
+        ul {
+            margin: 2rem 0 0 0;
+            li {
+                a {
+                    padding: 1.5rem 0;
+                    display: block;
+                    font-size: 1.8rem;
+                    color:${({ theme }) => theme.COLORS.TEXT_MENU}; 
+                }
+                &:nth-of-type(6){
+                    margin-bottom: 2rem;
+                }
+            }
+        }
+
+    }
+`
+
+export const Blur = styled.div`
+  position: absolute; 
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 68rem;
+  height: 70rem;
+  @media (max-width:1024px) {
+    max-width: 68rem;
+    width: 100%;
+  }
+`;
+ 
+
